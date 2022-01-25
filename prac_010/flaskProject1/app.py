8e9851ab5b36ba953a76c8e09f2be56fb41d2fce
Chenyuhan_Shen <chenyuhan.shen@my.jcu.edu.au>
2022-01-25 21:12:27 +0800

    Change name

--- prac_010/flaskProject1/app.py
+++ prac_010/flaskProject1/app.py
@@ -8,5 +8,16 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/greet')
+@app.route('/greet/<name>')
+def greet(name=""):
+    return "Hello {}".format(name)
+
+
+@app.route('/f/<celsius>')
+def greet(celsius=""):
+    return f"{celsius} Celsius is {float(celsius) * 9.0 / 5 + 32} Fahrenheit"
+
+
 if __name__ == '__main__':
     app.run()
